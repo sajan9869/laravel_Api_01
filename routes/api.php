5e493a1d89d86c70702df14c2d1b9702f7c08b87
sajan9869 <sajan.bajracharya22@gmail.com>
2024-02-08 23:49:09 +0545

    [Added]::APi

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,10 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\PostController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/posts', [PostController::class, 'index']);
+Route::post('/posts', [PostController::class, 'store']);
+Route::get('/posts/{id}', [PostController::class, 'show']);
+Route::put('/posts/{id}', [PostController::class, 'update']);
+Route::delete('/posts/{id}', [PostController::class, 'destroy']);
